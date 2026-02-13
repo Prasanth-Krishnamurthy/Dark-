0ac5f8f968db41eb9d391dada1433e9b425b4c17
Prasanth-K-Death <darklucifer605@gmail.com>
2026-02-13 15:17:47 +0530

    first commit

---
+++ Break_Statement.py
@@ -0,0 +1,8 @@
+a=[1,3,5,7,9]
+val=8
+for i in a:
+    if i==val:
+        print("Value found")
+        break
+else:
+    print("Value not found")
---
+++ list_count.py
@@ -0,0 +1,3 @@
+list1 = ["apple", "banana", "cherry","apple"]
+list2 = list1.count("apple")
+print(list2)
